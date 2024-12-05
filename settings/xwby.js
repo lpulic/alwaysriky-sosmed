1fcadc9d4245de48f85a80ab19e9b821ebbf4b59
lpulic <lolilovers29@gmail.com>
2024-12-05 19:59:57 +0700

    Add files via upload

---
+++ settings/xwby.js
@@ -0,0 +1,13 @@
+// /settings/xwby.js
+
+const profileSettings = {
+  name: "ALWAYSRIKY COOL", // Nama pengguna
+  description: "Website media sosial🛍", // Deskripsi singkat
+  profilePicture: "/media/profile.jpg", // URL foto profil
+  links: [
+    { label: "Developer script", url: "https://wa.me/6282312934025" }, // Tambahkan link
+    { label: "Marga Xesonyc", url: "https://whatsapp.com/channel/0029Vai2HWf5q08iw5ZAZo0v" },
+    { label: "Channel Whatsapp", url: "https://chat.whatsapp.com/BZ7fR8mLsfm0gxQWxdHPFj" },
+    { label: "Bot Asisten Ai", url: "https://wa.me/6289653935371" }
+  ]
+};
